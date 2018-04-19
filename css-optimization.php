47fc42a18fe9e25597fc855b9bd109dc93c07773
optimalisatie <info@optimalisatie.nl>
2018-04-19 17:30:23 +0200

    Github Updater

--- css-optimization.php
+++ css-optimization.php
@@ -12,7 +12,7 @@ namespace O10n;
  * @wordpress-plugin
  * Plugin Name:       CSS Optimization
  * Description:       Advanced CSS optimization toolkit. Critical CSS, minification, concatenation, async loading, advanced editor, CSS Lint, Clean CSS (professional), beautifier and more.
- * Version:           0.0.57
+ * Version:           0.0.58
  * Author:            Optimization.Team
  * Author URI:        https://optimization.team/
  * GitHub Plugin URI: https://github.com/o10n-x/wordpress-css-optimization
@@ -30,7 +30,7 @@ if (defined('WP_INSTALLING') && WP_INSTALLING) {
 }
 
 // settings
-$module_version = '0.0.57';
+$module_version = '0.0.58';
 $minimum_core_version = '0.0.38';
 $plugin_path = dirname(__FILE__);
 
